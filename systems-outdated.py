589a4d85f47bef3d49c2090e15c43de93819d4e0
Athanasius <code@miggy.org>
2021-06-27 20:35:05 +0100

    systems-outdated: log which checks we're doing

--- systems-outdated.py
+++ systems-outdated.py
@@ -109,14 +109,17 @@ def main():
       exit(-3)
 
     if args.active_conflicts:
+      logger.info('Checking for stale systems with known active conflicts...')
       tourist_systems.extend(bgs.active_conflicts_needing_update(faction_id, since))
 
     if args.possible_losing_conflicts:
+      logger.info('Checking for stale systems with possible losing active conflicts...')
       tourist_systems.extend(bgs.possible_losing_conflicts(since, faction_id=faction_id, tick_plus=args.tick_plus))
 
     # Anywhere that was last seen with 'close' inf% to another MF and not
     # updated this tick.
     if args.danger_of_conflicts:
+      logger.info('Checking for stale systems with possible active conflicts...')
       tourist_systems.extend(bgs.stale_danger_of_conflicts(since, faction_id))
 
   else:
