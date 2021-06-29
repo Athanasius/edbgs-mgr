e13991e9cfe99575c7aecd72a8dd5f7007dd3dfb
Athanasius <code@miggy.org>
2021-06-29 16:48:54 +0100

    Django: Remove what looks like the Django secret key
    The auto-generated file is only for reference, so stop Git Guardian from
    panicking us.

--- edbgs_django/settings/auto-generated.py
+++ edbgs_django/settings/auto-generated.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lkk@%v8&u+)y6p4&hd!3x+7_8u+-ygt*e6&d0m%yp8ko)^t!p3'
+SECRET_KEY = 'NO NOT THE REAL ONE'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
